1e7b5e7616e4d63d2d5aa403e0e8c37a10bb92aa
hokage-216 <berrylevente@gmail.com>
2024-06-29 15:51:52 -0400

    geolocation functionality update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
 import { useState, useEffect } from 'react';
+import 'dotenv/config'
 import './App.css';
 
-const userLocation = {
-  long: 0,
-  lat: 0,
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      alert("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      alert("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred.");
+      break;
+  }
 }
 
-async function getUserLocation() {
-  const newLocation = navigator.geolocation.getCurrentPosition((position) => {
+function getUserLocation() {
+  
+  const userLocation = {
+    long: 0,
+    lat: 0,
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
     userLocation.long = position.coords.longitude;
     userLocation.lat = position.coords.latitude;
-  });
+  }, (error) => showError(error));
 
-  console.log(userLocation);
+  return userLocation;
 }
 
-function App() {
+export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <>
-      <div onLoad={() => {getUserLocation()}}></div>
-
-      <button type="button" onClick={() => {getUserLocation()}}>Find Location</button>
+      <button type="button" onClick={() => {setData(getUserLocation())}}>Find Location</button>
     </>
   )
 }
-
-export default App
